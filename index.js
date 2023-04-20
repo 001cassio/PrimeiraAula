fdc3b6d5e8e237f7db887f6e6f82933c57ba1ba1
001cassio <cassiooliveira203@gmail.com>
2023-04-19 22:00:49 -0300

    Aula 19-04

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -16,6 +17,21 @@ server.listen(port,hostname,() => {
     console.log(`Servidor rodando em http://${hostname}:${port}/`)
 })  
 
+*/
 
-teste 
+const express = require('express');
+const app=express();
+const path=require('path');
+const router=express.Router();
 
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+router.get('/about',function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html'));
+})
+
+app.use('/',router);
+app.use('/about',router);
+app.listen(process.env.port||3000);
+console.log("Servidor rodando");
